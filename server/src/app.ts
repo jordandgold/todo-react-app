6b2f40ce962cca6671f2efe15eb64725c87bab64
Jordan Gold <jordandgold@gmail.com>
2021-11-28 22:56:53 -0600

    set up client

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,13 +8,13 @@ const app: Express = express();
 const PORT: string | number = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(express.json());
 app.use(todoRoutes);
+
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.y8pkt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.set("useFindAndModify", false);
 
 mongoose
-  .connect(uri, options)
+  .connect(uri)
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server running on http://localhost:${PORT}`)
